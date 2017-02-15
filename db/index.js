3d505cf65ca69754350ca61e93dbd34548aac65b
Vipin Khushu <vipinkhushu@hotmail.com>
2017-02-16 00:25:38 +0530

    email bug fixed

--- db/index.js
+++ db/index.js
@@ -1080,25 +1080,26 @@ exports.register = function(req,res){
                             '            </tbody></table>'+
                             '          </body>'+
                             '          </html>';
+                            var from_email = new helper.Email('manantechnosurge@gmail.com','Elements Culmyca 2017');
+                            var to_email = new helper.Email(updatedinfoforqr.email);
+                            var subject = 'Registration Successful!';
+                            var content = new helper.Content("text/html", html);
+                            var mail = new helper.Mail(from_email, subject, to_email, content);
+
+                            var sg = require('sendgrid')('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');
+                            var request = sg.emptyRequest({
+                              method: 'POST',
+                              path: '/v3/mail/send',
+                              body: mail.toJSON(),
+                            });
+
+                            sg.API(request, function(error, response) {
+                              console.log(response.statusCode);
+                              console.log(response.body);
+                              console.log(response.headers);
+                            });
                         })
-var from_email = new helper.Email('manantechnosurge@gmail.com','Elements Culmyca 2017');
-var to_email = new helper.Email(updatedinfoforqr.email);
-var subject = 'Registration Successful!';
-var content = new helper.Content("text/html", html);
-var mail = new helper.Mail(from_email, subject, to_email, content);
 
-var sg = require('sendgrid')('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');
-var request = sg.emptyRequest({
-  method: 'POST',
-  path: '/v3/mail/send',
-  body: mail.toJSON(),
-});
-
-sg.API(request, function(error, response) {
-  console.log(response.statusCode);
-  console.log(response.body);
-  console.log(response.headers);
-});
                         //SENDING EMAIL SYSTEM ENDS
                     })
 })
